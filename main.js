c1bca41f3c669ff89f040380556173c0e33d9524
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2018-11-22 11:14:15 +0100

    creato progetto e pending creazione query

---
+++ main.js
@@ -0,0 +1,24 @@
+const { _: [sqlFileName], e = "local", env = e } = require("simple-argv")
+if (!sqlFileName) {
+  throw new Error("devi specificare il nome del file sql da eseguire come primo argomento del comando")
+}
+console.log(`Eseguo ${sqlFileName}.sql in ambiente ${env}`)
+const mysql = require("mysql")
+const config = require(`./db-config-${env}.json`)
+const { readFileSync } = require("fs")
+const { join } = require("path")
+
+const sql = readFileSync(join(__dirname, `${sqlFileName}.sql`), "utf8")
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  ...config
+})
+
+pool.query(sql, (err, data) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(data)
+  }
+})

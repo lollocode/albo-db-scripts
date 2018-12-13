38cc399699b50e3b93fb66aaea012e5095e8cc88
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2018-12-13 10:15:42 +0100

    fix in seeder e in tables

---
+++ insertSchools.sql
@@ -0,0 +1,17 @@
+INSERT INTO schools
+VALUES 
+(1, "moro"),
+(2, "spalla"),
+(3, "zanelli"),
+(4, "canossa"),
+(5, "chierici"),
+(6, "motti"),
+(7, "nobili"),
+(8, "iti"),
+(9, "ipsia"),
+(10, "bus"),
+(11, "levi"),
+(12, "secchi"),
+(13, "iodi"),
+(14, "galvani"),
+(15, "iess");
